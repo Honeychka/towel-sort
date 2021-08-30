9698564c53ebbd5f1dbcb08900979204099be7b0
Hanna Khanko <ahanko802@gmail.com>
2021-08-30 22:09:29 +0300

    Feat: Solution for sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+ let Sorted = [];
+   if (!matrix){
+    return Sorted;
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+		if (i % 2 == 0) {
+      Sorted.push(...matrix[i]);
+    } 
+		else {
+      Sorted.push(...matrix[i].reverse());
+    }
+	}
+	return Sorted;
+};
